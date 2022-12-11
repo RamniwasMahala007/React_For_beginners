22c5048107bfdc8629208dcbc78e645602bafa0b
RamniwasMahala007 <71785474+RamniwasMahala007@users.noreply.github.com>
2022-12-11 12:23:55 +0530

    adding readme file of first challenge with code

--- 01-challenge/src/index.js
+++ 01-challenge/src/index.js
@@ -1,10 +1,42 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
- 
-  <h1> Hello This is Ramniwas</h1>
-);
+const root = document.getElementById('root');
+
+
+const header = (
+  <header>
+    <h1>Welcome to React Basic Challenge</h1>
+    <h2>Getting Started React</h2>
+    <h3>JavaScript Library</h3>
+    <p> Hello This is Ramniwas first basic challenge</p>
+  </header>
+)
+
+
+const img1="https://picsum.photos/400/400";
+const img2="https://picsum.photos/350/400";
+const img3="https://picsum.photos/300/400";
+const img4="https://picsum.photos/250/400";
+
+
+const image = (
+<>
+  <img src={img1} alt="Images" />
+  <img src={img2} alt="Images" />
+  <img src={img3} alt="Images" />
+  <img src={img4} alt="Images" />
+</>
+)
+
+
+const wrapp =(
+  <>
+      {header}
+      {image}
+  </>
+  
+)
+ReactDOM.render(wrapp, root );
 
 
